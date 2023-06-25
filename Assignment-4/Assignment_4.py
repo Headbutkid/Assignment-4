c1e9acf7163fb92418035d0e472cddd8404c7a80
Headbutkid <devinrobinson20023@gmail.com>
2023-06-26 11:21:08 +1200

    All records in table show, update.

--- Assignment-4/Assignment_4.py
+++ Assignment-4/Assignment_4.py
@@ -5,15 +5,29 @@
 import pyodbc
 
 print("What do you want to do? (A, B, C OR D)")
-print("A- diplay all, B- display growth, C- display by date or D- range of dates")
+print("A - diplay all, B - display growth, C - display by date or D - range of dates")
 
 choice = input()
 
 def connect_to_db():
-    pass
+    conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\2022000425\Downloads\Company-202200425.accdb;')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * from Company_Data")
+
+   
 
 def print_all_records():
-    pass
+   conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\2022000425\Downloads\Company-202200425.accdb;')
+   cursor = conn.cursor()
+   cursor.execute("SELECT * from Company_Data")
+
+   print(f'{"Company":<20}{"Industry":<20}{"Year revenue":<20}{"Revenue Growth":<20}{"Employees":<20}{"Found date":<15}{"Headquarter":<0}')
+   print(f'{ "_"*130:<75}')
+
+   for row in cursor.fetchall():
+       record = f'{row.Company_Name:<20}{row.Industry:<20}{row.Year_revenue:<25}{row.Revenue_growth:<20}{row.Number_of_employees:<15}{row.Company_found_date:<15}{row.Headquarter:<0}'
+
+       print(record)
 
 def print_positive_growth():
     pass
